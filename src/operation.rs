da5738969be254494bba2428a6474f08ef8909d4
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2017-11-06 21:09:05 +0900

    Add interactive shell

--- src/operation.rs
+++ src/operation.rs
@@ -1,52 +1,52 @@
 use shrust::{Shell, ShellIO};
-use std::io::prelude::*;
 use cask::errors::Result;
 use std::str;
 use cask::Cask;
 use std::sync::Arc;
 
-/// get returns ...
+/// get gets the value from specified key.
 pub fn get(cask: Arc<Cask>, key: String) -> Result<()> {
     let v = cask.get(key)?;
-    println!("value:{}", str::from_utf8(&v.unwrap()).unwrap());
+    println!("{}", str::from_utf8(&v.unwrap()).unwrap());
     Ok(())
 }
 
-/// put returns...
+/// put puts the value with specified key.
 pub fn put(cask: Arc<Cask>, key: String, value: String) -> Result<()> {
     cask.put(key, value)?;
     Ok(())
 }
 
-/// delete returns...
+/// delete deletes the value with specified key.
 pub fn delete(cask: Arc<Cask>, key: String) -> Result<()> {
     cask.delete(key)?;
     Ok(())
 }
 
 /// interactive starts interactive shell for cask.
-///
-// TODO: argment
 pub fn interactive(cask: Arc<Cask>) -> Result<()> {
-    let mut shell = Shell::new(());
+    let mut shell = Shell::new((cask.clone()));
 
-    //  create
-    shell.new_command_noargs(
-        "delete",
-        "delete the value with specified key",
-        |io, _, key| {
-            //        delete(cask, key
-            try!(writeln!(io, "Hello World !!!"));
-            Ok(())
-        },
-    );
-
-    // get
-    shell.new_command("get", "List strings", 1, move |io, _, key| {
-        let v = cask.get(key[0].to_string())?;
-        try!(writeln!(io, "{:?}", v));
+    //  delete
+    shell.new_command("delete", "delete the value with specified key", 1, |_,
+     cask,
+     key| {
+        delete(cask.clone(), key[0].to_string())?;
         Ok(())
     });
+
+    // get
+    shell.new_command("get", "List strings", 1, move |_, cask, key| {
+        get(cask.clone(), key[0].to_string())?;
+        Ok(())
+    });
+
+    // put
+    shell.new_command("put", "List strings", 2, move |_, cask, key| {
+        put(cask.clone(), key[0].to_string(), key[1].to_string())?;
+        Ok(())
+    });
+
     shell.run_loop(&mut ShellIO::default());
     Ok(())
 }
